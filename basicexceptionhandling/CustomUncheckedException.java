8ba2f94ee5a1812f1301b72ab145e0e86755bcc1
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-01 15:34:01 +0530

    Custom unchecked exception

---
+++ basicexceptionhandling/CustomUncheckedException.java
@@ -0,0 +1,30 @@
+package basicexceptionhandling;
+
+import java.util.Scanner;
+
+class NegativeNumberException extends Exception
+{
+    @Override
+    public String getMessage() {
+        return "Negative number not permissible!";
+    }
+}
+
+public class CustomUncheckedException
+{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        try
+        {
+            int number=scanner.nextInt();
+            if(number<0)
+                throw new NegativeNumberException();
+            System.out.println("Valid integer!");
+        }
+        catch (NegativeNumberException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
