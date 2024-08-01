bb70f0a723c07364b3219f8ddc2f6af39177c4f9
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-01 15:37:49 +0530

    Exception chaining

---
+++ basicexceptionhandling/ExceptionChaining.java
@@ -0,0 +1,29 @@
+package basicexceptionhandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ExceptionChaining
+{
+    public static void ioExceptionMethod(String filename) throws IOException
+    {
+        System.out.println("Trying to open"+filename);
+        BufferedReader reader=new BufferedReader(new FileReader(filename));
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the file name:");
+        String filename=scanner.next();
+        try
+        {
+            ioExceptionMethod(filename);
+        }catch (IOException e)
+        {
+            System.out.println("File not found.");
+            System.out.println("Exception thrown by method handled in main!");
+        }
+    }
+}
